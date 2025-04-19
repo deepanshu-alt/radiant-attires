2f7ebaf46a170a606c1263f9ea33c2f0639e7f79
deepanshu-alt <88389527+deepanshu-alt@users.noreply.github.com>
2025-04-19 17:20:10 +0530

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
 Route::get('/registration', function(){
     return view('registration');
 });
+
+Route::get('/login', function(){
+    return view('login');
+});
+
+
+Route::get('/signup', function(){
+    return view('signup');
+});
